84a03589b9d297cf4c59e63dc802e469e39f7daa
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-23 14:42:42 +0800

    Update GuiHandler.java

--- src/main/java/meldexun/better_diving/network/GuiHandler.java
+++ src/main/java/meldexun/better_diving/network/GuiHandler.java
@@ -1,44 +1,19 @@
 package meldexun.better_diving.network;
 
-import meldexun.better_diving.client.gui.GuiFabricator;
-import meldexun.better_diving.client.gui.GuiHabitatBuilder;
-
-import meldexun.better_diving.container.ContainerFabricator;
-
-
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
-import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
 
-	
-	
-	public static final int GUI_FABRICATOR = 0;
-	public static final int GUI_HABITAT_BUILDER = 1;
-
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		switch (ID) {
-		case GUI_FABRICATOR:
-			return new ContainerFabricator(player.inventory, player);
-		default:
-			return null;
-		}
+		return null;
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		switch (ID) {
-		case GUI_FABRICATOR:
-			return new GuiFabricator((Container) this.getServerGuiElement(ID, player, world, x, y, z));
-		case GUI_HABITAT_BUILDER:
-			return new GuiHabitatBuilder();
-		default:
-			return null;
-		}
+		return null;
 	}
 
 }
