b5af7c1bfebd5501ee0d9aed31741ab9823f994e
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-23 12:34:04 +0800

    Update EntityModels.java

--- src/main/java/meldexun/better_diving/client/EntityModels.java
+++ src/main/java/meldexun/better_diving/client/EntityModels.java
@@ -7,13 +7,12 @@ import meldexun.better_diving.client.model.entity.ModelGarryfish;
 import meldexun.better_diving.client.model.entity.ModelHolefish;
 import meldexun.better_diving.client.model.entity.ModelPeeper;
 import meldexun.better_diving.client.renderer.entity.RenderFish;
-import meldexun.better_diving.client.renderer.entity.RenderSeamoth;
+
 import meldexun.better_diving.entity.EntityBladderfish;
 import meldexun.better_diving.entity.EntityBoomerang;
 import meldexun.better_diving.entity.EntityGarryfish;
 import meldexun.better_diving.entity.EntityHolefish;
 import meldexun.better_diving.entity.EntityPeeper;
-import meldexun.better_diving.entity.EntitySeamoth;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -31,7 +30,6 @@ public class EntityModels {
 
 	@SubscribeEvent
 	public static void registerEntityRenders(ModelRegistryEvent event) {
-		RenderingRegistry.registerEntityRenderingHandler(EntitySeamoth.class, RenderSeamoth::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityPeeper.class, manager -> new RenderFish(manager, new ModelPeeper(), 0.4F, "peeper"));
 		RenderingRegistry.registerEntityRenderingHandler(EntityBladderfish.class, manager -> new RenderFish(manager, new ModelBladderfish(), 0.4F, "bladderfish"));
 		RenderingRegistry.registerEntityRenderingHandler(EntityGarryfish.class, manager -> new RenderFish(manager, new ModelGarryfish(), 0.4F, "garryfish"));
