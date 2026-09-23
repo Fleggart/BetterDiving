44efc0a5c74e2f18f4794adb32493e309f47fd7c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-23 13:31:37 +0800

    Update ArtemisLib.java

--- src/main/java/meldexun/better_diving/integration/ArtemisLib.java
+++ src/main/java/meldexun/better_diving/integration/ArtemisLib.java
@@ -1,14 +1,11 @@
 package meldexun.better_diving.integration;
 
 import org.lwjgl.opengl.GL11;
-
 import com.artemis.artemislib.compatibilities.sizeCap.ISizeCap;
 import com.artemis.artemislib.compatibilities.sizeCap.SizeCapPro;
 import com.artemis.artemislib.util.attributes.ArtemisLibAttributes;
-
 import meldexun.better_diving.capability.diving.CapabilityDivingAttributesProvider;
 import meldexun.better_diving.capability.diving.ICapabilityDivingAttributes;
-import meldexun.better_diving.entity.EntitySeamoth;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
