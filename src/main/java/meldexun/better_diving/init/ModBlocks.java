2f7246e5cc7287f24b9965bd73184ab29cae52d9
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-24 17:20:14 +0800

    Update ModBlocks.java

--- src/main/java/meldexun/better_diving/init/ModBlocks.java
+++ src/main/java/meldexun/better_diving/init/ModBlocks.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nullable;
-
 import meldexun.better_diving.BetterDiving;
 import meldexun.better_diving.block.BlockCreepvine;
 import meldexun.better_diving.block.BlockCreepvineSeed;
@@ -14,8 +13,6 @@ import meldexun.better_diving.block.BlockSeagrassTall;
 import meldexun.better_diving.block.BlockUnderwaterBlock;
 import meldexun.better_diving.item.ItemBlockCreepvine;
 import meldexun.better_diving.item.ItemBlockTooltip;
-import meldexun.better_diving.tileentity.TileEntityCreepvine;
-import meldexun.better_diving.tileentity.TileEntityCreepvineTop;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -78,8 +75,6 @@ public class ModBlocks {
 				BlockRegistrationHandler.BLOCKS.add(block);
 			}
 
-			GameRegistry.registerTileEntity(TileEntityCreepvine.class, new ResourceLocation(BetterDiving.MOD_ID, "tile_entity_creepvine"));
-			GameRegistry.registerTileEntity(TileEntityCreepvineTop.class, new ResourceLocation(BetterDiving.MOD_ID, "tile_entity_creepvine_top"));
 		}
 
 		private static Block setBlockName(Block block, String name) {
