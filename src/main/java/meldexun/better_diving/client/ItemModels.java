0ef9a76ae47b4e55524df13f03bfb7bc9f24706b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-26 20:22:45 +0800

    Update ItemModels.java

--- src/main/java/meldexun/better_diving/client/ItemModels.java
+++ src/main/java/meldexun/better_diving/client/ItemModels.java
@@ -2,18 +2,10 @@ package meldexun.better_diving.client;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import meldexun.better_diving.BetterDiving;
-import meldexun.better_diving.init.ModBlocks;
 import meldexun.better_diving.init.ModItems;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockLiquid;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -30,7 +22,7 @@ public class ItemModels {
 
 	}
 
-	private static StateMap.Builder builder = new StateMap.Builder();
+	
 	private static final List<Item> REGISTERED_ITEM_MODELS = new ArrayList<>();
 
 	@SubscribeEvent
@@ -40,19 +32,6 @@ public class ItemModels {
 				ItemModels.registerItemModel(item);
 			}
 		}
-
-		for (ItemBlock itemBlock : ModBlocks.BlockRegistrationHandler.ITEM_BLOCKS) {
-			if (!ItemModels.REGISTERED_ITEM_MODELS.contains(itemBlock)) {
-				ItemModels.registerItemModel(itemBlock);
-			}
-		}
-
-		for (Block block : ModBlocks.BlockRegistrationHandler.BLOCKS) {
-			if (block.getDefaultState().getMaterial() == Material.WATER) {
-				ItemModels.ignoreProperty(block, BlockLiquid.LEVEL);
-			}
-		}
-		
 	}
 
 	private static void registerItemModel(Item item) {
@@ -62,10 +41,6 @@ public class ItemModels {
 	private static void registerCustomItemModel(Item item, int meta, ResourceLocation modelLocation, String variant) {
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(modelLocation, variant));
 		ItemModels.REGISTERED_ITEM_MODELS.add(item);
-	}
-
-	private static void ignoreProperty(Block block, IProperty property) {
-		ModelLoader.setCustomStateMapper(block, ItemModels.builder.ignore(property).build());
 	}
 
 }
